5462bf15aae797b747af1abd4a266203996ae07c
Webster Fever <webstersevenlanguages@gmail.com>
2026-05-20 01:26:49 -0400

    add front

---
+++ front/types/index.ts
@@ -0,0 +1,14 @@
+interface ICategory {
+  id: string;
+  name: string;
+}
+
+export interface IProduct {
+  id: string;
+  name: string;
+  description: string;
+  price: string;
+  stock: number;
+  imgUrl: string;
+  category: ICategory;
+}
